08f5c9a78e66049a02f7d205605a5b7963183e6d
Bogdan Dobrin <b.dobrin@yahoo.com>
2021-11-07 21:42:56 +0200

    Dashboard now changes depending on user role. Added logo on login, register and reset page.

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -59,31 +59,35 @@ function Dashboard() {
 				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
 				<Navbar.Collapse id="responsive-navbar-nav">
 					<Nav className="me-auto">
-					<Nav.Link eventKey="disabled" disabled>Logged in as {name}</Nav.Link>
-					<Nav.Link href="#newoffers">New Offers</Nav.Link>
-					<NavDropdown title="My Transports" id="collasible-nav-dropdown">
-						<NavDropdown.Item href="#transport/1">B100TNG Arad-Bucuresti</NavDropdown.Item>
-						<NavDropdown.Item href="#transport/2">B200TNG Bucuresti-Constanta</NavDropdown.Item>
-						<NavDropdown.Item href="#transport/3">B300TNG Sibiu-Brasov</NavDropdown.Item>
-						<NavDropdown.Divider />
-						<NavDropdown.Item href="#action/3.4">Finished Transports</NavDropdown.Item>
-					</NavDropdown>
+					<Nav.Link eventKey="disabled" disabled>Logged in as {name} | {role}</Nav.Link>
+					{
+						role === "Client" ? (
+							<React.Fragment>
+							<Nav.Link href="#ship">Ship</Nav.Link>
+							<Nav.Link href="#shipments">My Shipments</Nav.Link>
+							</React.Fragment>
+						) : (
+							<React.Fragment>
+							<Nav.Link href="#newoffers">New Offers</Nav.Link>
+							<NavDropdown title="My Transports" id="collasible-nav-dropdown">
+								<NavDropdown.Item href="#transport/1">B100TNG Arad-Bucuresti</NavDropdown.Item>
+								<NavDropdown.Item href="#transport/2">B200TNG Bucuresti-Constanta</NavDropdown.Item>
+								<NavDropdown.Item href="#transport/3">B300TNG Sibiu-Brasov</NavDropdown.Item>
+								<NavDropdown.Divider />
+								<NavDropdown.Item href="#action/3.4">Finished Transports</NavDropdown.Item>
+							</NavDropdown>
+							</React.Fragment>
+						)
+					}
 					</Nav>
 					<Nav>
 					<Nav.Link href="#settings">Settings</Nav.Link>
 					<Nav.Link eventKey={2} href="#contact">Contact</Nav.Link>
+					<Nav.Link onClick={logout}>Logout</Nav.Link>
 					</Nav>
 				</Navbar.Collapse>
 				</Container>
 			</Navbar>
-			<div className="dashboard">
-				<div className="dashboard__container">Logged in as
-					<div>{name}</div>
-					<div>{role}</div>
-					<div>{user?.email}</div>
-					<button className="dashboard__btn" onClick={logout}>Logout</button>
-				</div>
-			</div>
 		</div>
 		
 	);
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { auth, signInWithEmailAndPassword } from "../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
+import logo from './../assets/delivery.png';
 import "./Login.css";
 
 function Login() {
@@ -33,6 +34,10 @@ function Login() {
 	return (
 		<div className="login">
 			<div className="login__container">
+				<div >
+					<img src={logo} alt="TransNG Logo" width="64px" height="64px" />
+					<p style={{ fontSize: '24px', fontWeight: 'bold' }} >TransNG</p>
+				</div>
 				<input type="text" className="login__textBox" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="E-mail Address" />
 				<input type="password" className="login__textBox" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
 				<button className="login__btn" onClick={() => signInWithEmailAndPassword(email, password)}>Login</button>
