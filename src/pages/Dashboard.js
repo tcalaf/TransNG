21ca7a60b802f9395976b800205142fb91dcf54d
Bogdan Dobrin <b.dobrin@yahoo.com>
2021-11-07 19:03:53 +0200

    Added navbar in dashboard.

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,8 +1,14 @@
 import React, { useEffect, useState, useCallback } from "react";
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useHistory } from "react-router";
 import "./Dashboard.css";
 import { auth, db, logout } from "../firebase";
+import logo from './../assets/delivery_light.png';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Dashboard() {
 	const [user, loading, error] = useAuthState(auth);
@@ -44,14 +50,42 @@ function Dashboard() {
 	}, [user, loading, error, history, fetchData]);
 
 	return (
-		<div className="dashboard">
-			<div className="dashboard__container">Logged in as
-				<div>{name}</div>
-				<div>{role}</div>
-				<div>{user?.email}</div>
-				<button className="dashboard__btn" onClick={logout}>Logout</button>
+		<div>
+			<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+				<Container>
+				<Navbar.Brand href="#home">
+					<img src={logo} alt="TransNG Logo" width="30" height="30"/>{' '}TransNG
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+				<Navbar.Collapse id="responsive-navbar-nav">
+					<Nav className="me-auto">
+					<Nav.Link eventKey="disabled" disabled>Logged in as {name}</Nav.Link>
+					<Nav.Link href="#newoffers">New Offers</Nav.Link>
+					<NavDropdown title="My Transports" id="collasible-nav-dropdown">
+						<NavDropdown.Item href="#transport/1">B100TNG Arad-Bucuresti</NavDropdown.Item>
+						<NavDropdown.Item href="#transport/2">B200TNG Bucuresti-Constanta</NavDropdown.Item>
+						<NavDropdown.Item href="#transport/3">B300TNG Sibiu-Brasov</NavDropdown.Item>
+						<NavDropdown.Divider />
+						<NavDropdown.Item href="#action/3.4">Finished Transports</NavDropdown.Item>
+					</NavDropdown>
+					</Nav>
+					<Nav>
+					<Nav.Link href="#settings">Settings</Nav.Link>
+					<Nav.Link eventKey={2} href="#contact">Contact</Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
+				</Container>
+			</Navbar>
+			<div className="dashboard">
+				<div className="dashboard__container">Logged in as
+					<div>{name}</div>
+					<div>{role}</div>
+					<div>{user?.email}</div>
+					<button className="dashboard__btn" onClick={logout}>Logout</button>
+				</div>
 			</div>
 		</div>
+		
 	);
 }
 export default Dashboard;
