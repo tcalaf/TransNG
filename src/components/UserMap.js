2a2080ef2dec1a22153f0310787f7505ae80b49d
Stefania Damian <52752630+Stefania12@users.noreply.github.com>
2022-01-10 12:39:56 +0200

    Added a truck route layer to map (#5)

--- src/components/UserMap.js
+++ src/components/UserMap.js
@@ -1,114 +1,60 @@
 import './UserMap.css';
-import React, { useRef, useEffect, useState } from "react";
+import React, { useRef, useEffect, useState, useCallback } from "react";
 import Map from "@arcgis/core/Map";
 import MapView from "@arcgis/core/views/MapView";
 import GraphicsLayer from "@arcgis/core/layers/GraphicsLayer";
-import Graphic from "@arcgis/core/Graphic";
-import Point from "@arcgis/core/geometry/Point";
-import SimpleMarkerSymbol from "@arcgis/core/symbols/SimpleMarkerSymbol";
-import PopupTemplate from "@arcgis/core/PopupTemplate";
 
-const newTruckGraphic = (coords, color, attributes = {}) => {
-  return new Graphic({
-    geometry: new Point({
-      x: coords.x,
-      y: coords.y,
-    }),
-    symbol: new SimpleMarkerSymbol({
-      color,
-      size: "10px",
-      outline: {
-        color
-      }
-    }),
-    attributes,
-    popupTemplate: new PopupTemplate({
-      title: "Truck ID: {id_truck}",
-      content: [
-        {
-          type: "fields",
-          fieldInfos: [
-            {
-              label: "From",
-              fieldName: "start_place",
-            },
-            {
-              label: "To",
-              fieldName: "finish_place",
-            },
-          ]
-        },
-        {
-          type: "fields",
-          fieldInfos: [
-            {
-              label: "Departure date",
-              fieldName: "start_date",
-            },
-            {
-              label: "Arrival date",
-              fieldName: "finish_date",
-            },
-          ]
-        }
-      ]
-    })
-  })
+import { fetchRouteDetails, getDBSupplies, newTruckGraphic } from './utils';
+
+const getRandomInt = (max) => {
+  return Math.floor(Math.random() * max);
 }
 
-const getDBSupplies = () => {
-  // TODO: replace with db call to supplies
-  let supplies = [
-    {
-      id_truck: "smth",
-      start_date: "26 Dec 2021 12:00:00",
-      start_place: "aaa2",
-      finish_date: "bbb",
-      finish_place: "bbb2",
-      current_place: '{"x": -118.475, "y": 34.026}'
-    },
-    {
-      id_truck: "smth2",
-      start_date: "20 Jan 2022 04:00:00",
-      start_place: "ccc2",
-      finish_date: "ddd",
-      finish_place: "ddd2",
-      current_place: '{"x": -118.475, "y": 34.526}'
+const getRouteLayer = (features) => {
+  const routeLayer = new GraphicsLayer();
+  for (let f of features) {
+    f.symbol = {
+      type: "simple-line",
+      color: [getRandomInt(200), getRandomInt(200), getRandomInt(200), 0.75],
+      width: "4px"
     }
-  ];
-
-  return supplies.map((x) => {
-    return newTruckGraphic(
-      JSON.parse(x.current_place),
-      (Date.parse(x.start_date) < Date.now()) ? "red" : "green",
-      x)
-  }, supplies);
+  }
+  routeLayer.addMany(features);
+  return routeLayer;
 }
 
-const UserMap = (props) => {
+const UserMap = () => {
     const mapDiv = useRef(null);
-    const [supplies, setSupplies] = useState(null);
     const [view, setView] = useState(null);
 
     // Init map parameters.
     useEffect(() => {
-      const newSupplies = getDBSupplies();
-
-      setSupplies(newSupplies);
-      setView(new MapView({
-        container: mapDiv.current,
-        map: new Map({
-          basemap: "arcgis-navigation",
-          layers: [
-            new GraphicsLayer({
-              id: "Trucks Points",
-              graphics: newSupplies
-            })
-          ]
-        }),
-        scale: 1000000,
-        center: [-118.475, 34.026],
-      }));
+      const supplies = getDBSupplies();
+      const suppliesGraphics = supplies.map((x) => {
+        return newTruckGraphic(
+          JSON.parse(x.current_place),
+          (Date.parse(x.start_date) < Date.now()) ? "red" : "green",
+          x)
+      });
+      
+      fetchRouteDetails(supplies[0], [{start_place: "Beverly Hills", finish_place: "Inglewood"}]).then((val) => {
+        console.log(val);
+        setView(new MapView({
+          container: mapDiv.current,
+          map: new Map({
+            basemap: "arcgis-navigation",
+            layers: [
+              new GraphicsLayer({
+                id: "Trucks Points",
+                graphics: suppliesGraphics
+              }),
+              getRouteLayer(val.results[2].value.features)
+            ]
+          }),
+          scale: 1000000,
+          center: [-118.475, 34.026],
+        }));
+      });
     }, []);
 
     return <div className="mapDiv" ref={mapDiv}></div>;
--- src/components/utils.js
+++ src/components/utils.js
@@ -1,23 +1,173 @@
 import * as locator from "@arcgis/core/rest/locator";
+import * as geoprocessor from "@arcgis/core/rest/geoprocessor";
+import Graphic from "@arcgis/core/Graphic";
+import Point from "@arcgis/core/geometry/Point";
+import SimpleMarkerSymbol from "@arcgis/core/symbols/SimpleMarkerSymbol";
+import PopupTemplate from "@arcgis/core/PopupTemplate";
+import FeatureSet from "@arcgis/core/rest/support/FeatureSet";
 
 const locatorUrl = "https://geocode-api.arcgis.com/arcgis/rest/services/World/GeocodeServer";
+const fleetRoutingUrl = "https://logistics.arcgis.com/arcgis/rest/services/World/VehicleRoutingProblemSync/GPServer/EditVehicleRoutingProblem";
 
-
-export const addressesToCoordinates = (addresses) => {
+export const addressesToCoordinates = async (addresses) => {
     const coordinates = [];
-
-    for (const addr in addresses) {
-        locator.addressToLocations(
-        locatorUrl, {
-        address: {
-            "address": addr
-        }
-        }).then((results) => {
+    for (const addr of addresses) {
+        const results = await locator.addressToLocations(
+            locatorUrl, {
+            address: {"SingleLine": addr}
+        });
+        
         coordinates.push({
             x: results[0].location.x,
             y: results[0].location.y
         });
-        });
     }
+    console.log(coordinates);
     return coordinates;
 };
+
+export const newTruckGraphic = (coords, color, attributes = {}) => {
+    return new Graphic({
+      geometry: new Point({
+        x: coords.x,
+        y: coords.y,
+      }),
+      symbol: new SimpleMarkerSymbol({
+        color,
+        size: "10px",
+        outline: {
+          color
+        }
+      }),
+      attributes,
+      popupTemplate: new PopupTemplate({
+        title: "Truck ID: {id_truck}",
+        content: [
+          {
+            type: "fields",
+            fieldInfos: [
+              {
+                label: "From",
+                fieldName: "start_place",
+              },
+              {
+                label: "To",
+                fieldName: "finish_place",
+              },
+            ]
+          },
+          {
+            type: "fields",
+            fieldInfos: [
+              {
+                label: "Departure date",
+                fieldName: "start_date",
+              },
+              {
+                label: "Arrival date",
+                fieldName: "finish_date",
+              },
+            ]
+          }
+        ]
+      })
+    })
+  }
+  
+export const getDBSupplies = () => {
+    // TODO: replace with db call to supplies
+    let supplies = [
+        {
+        id_truck: "smth",
+        start_date: "26 Dec 2021 12:00:00",
+        start_place: "Malibu",
+        finish_date: "22 Jan 2022 04:00:00",
+        finish_place: "Los Angeles International Airport",
+        current_place: '{"x": -118.475, "y": 34.026}'
+        },
+        {
+        id_truck: "smth2",
+        start_date: "20 Jan 2022 04:00:00",
+        start_place: "ccc2",
+        finish_date: "ddd",
+        finish_place: "ddd2",
+        current_place: '{"x": -118.475, "y": 34.526}'
+        }
+    ];
+
+    return supplies;
+}
+
+
+// supply is data from db, demands is array of demands as from db
+export const fetchRouteDetails = async (supply, demands) => {
+    const locations = [supply.start_place, supply.finish_place];
+    for (const d of demands) {
+        locations.push(d.start_place);
+        locations.push(d.finish_place);
+    }
+    const coords = await addressesToCoordinates(locations);
+
+    const depots = [
+        {
+            attributes: {Name: supply.start_place},
+            geometry: {type: "point", ...coords[0]}
+        },
+        {
+            attributes: {Name: supply.finish_place},
+            geometry: {type: "point", ...coords[1]}
+        }
+    ];
+    const order_pairs = [];
+    const orders = [{
+        attributes: {Name: supply.finish_place},
+        geometry: {type: "point", ...coords[1]}
+    }];
+
+    for (let i = 2; i < locations.length; i = i + 2) {
+        order_pairs.push({
+            attributes: {
+                FirstOrderName: locations[i],
+                SecondOrderName: locations[i+1]
+            }
+        });
+
+        orders.push({
+            attributes: {
+                Name: locations[i],
+                DeliveryQuantities: null,
+                PickupQuantities: 1
+            },
+            geometry: {type: "point", ...coords[i]}
+        });
+        orders.push({
+            attributes: {
+                Name: locations[i+1],
+                PickupQuantities: null,
+                DeliveryQuantities: 1
+            },
+            geometry: {type: "point", ...coords[i+1]}
+        });
+    }
+
+    const routes = new FeatureSet({
+        features: [{
+            attributes: {
+                Name: "Route 1",
+                Description: "vehicle 1",
+                StartDepotName: supply.start_place,
+                EndDepotName: supply.finish_place,
+                Capacities: "4",
+            }
+        }]
+    });
+
+    const params = {
+        orders: new FeatureSet({features: orders}),
+        depots: new FeatureSet({features: depots}),
+        routes,
+        order_pairs: new FeatureSet({features: order_pairs})
+    };
+
+    return await geoprocessor.execute(fleetRoutingUrl, params);
+}
